42b0363ea60c015845aa7f436ebdd95793e0caea
Zachary Chavez <zach@rychavez.com>
2012-11-23 22:46:55 -0700

    Added basic get_related_files tests.

--- file_selector_tests.py
+++ file_selector_tests.py
@@ -2,7 +2,16 @@ import sublime
 import unittest
 from GotoRelatedFile import FileSelector
 
+import os
+
+
 class TestFileSelector(unittest.TestCase):
+
+    def setUp(self):
+        base_path = os.sep.join([os.getcwd(), 'test_data', 'application'])
+        self.controller_path = os.sep.join([base_path, 'controllers', 'foo.py'])
+        self.view_path = os.sep.join([base_path, 'views', 'foo', 'bar.py'])
+        self.template_path = os.sep.join([base_path, 'templates', 'foo', 'bar.html'])
 
     def testConfigurationSetToFirstMatchingAppPath(self):
         # Path matches config2 and config3, but 2 is first.
@@ -29,3 +38,34 @@ class TestFileSelector(unittest.TestCase):
             file_selector.configuration,
             file_selector.settings.get('config3')
         )
+
+    def testBasicRelPatternTemplateVars(self):
+        # Test template vars app_path, type_path,
+        # file_from_type_path, and dir_from_type_path
+        file_selector = FileSelector(
+            sublime.active_window(),
+            'test_settings.sublime-settings',
+            self.view_path
+        )
+
+        self.assertTrue(file_selector.files_found)
+
+        related_files = file_selector.related_files
+        self.assertEquals(len(related_files), 2)
+        self.assertEquals(related_files[0][1], self.controller_path)
+        self.assertEquals(related_files[1][1], self.template_path)
+
+    def testRelPatternsCanContainAsteriskWildcardCharacter(self):
+        file_selector = FileSelector(
+            sublime.active_window(),
+            'test_settings.sublime-settings',
+            self.controller_path
+        )
+
+        self.assertTrue(file_selector.files_found)
+
+        related_files = file_selector.related_files
+        self.assertEquals(len(related_files), 2)
+        self.assertEquals(related_files[0][1], self.template_path)
+        self.assertEquals(related_files[1][1], self.view_path)
+
